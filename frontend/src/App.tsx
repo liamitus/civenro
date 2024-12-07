c5feefbcee2f32c556606234b8ef6f89140b7449
Liam Howell <liammm@gmail.com>
2024-12-07 15:30:27 -0500

    feat: wrap App component in ThemeProvider for consistent theming across the application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+// frontend/src/App.tsx
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import BillDetailPage from './pages/BillDetailPage';
@@ -8,27 +10,54 @@ import { ModalProvider } from './context/ModalContext';
 import { UserProvider } from './context/UserContext';
 import { LoadScript } from '@react-google-maps/api';
 import AccountPage from './pages/AccountPage';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0A1F44',
+    },
+    background: {
+      default: '#F5F5F5',
+    },
+    text: {
+      primary: '#333333',
+    },
+    secondary: {
+      main: '#A3895E',
+    },
+    success: {
+      main: '#1B8062',
+    },
+    error: {
+      main: '#B23A48',
+    },
+  },
+});
 
 function App() {
   return (
-    <AuthProvider>
-      <UserProvider>
-        <ModalProvider>
-          {/* <LoadScript googleMapsApiKey="YOUR_GOOGLE_MAPS_API_KEY"> */}
-          {/* <GoogleMapsLoader> */}
-          <Router>
-            <NavBar />
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/user/:userId" element={<AccountPage />} />
-              <Route path="/bill/:id" element={<BillDetailPage />} />
-            </Routes>
-          </Router>
-          {/* </GoogleMapsLoader> */}
-          {/* </LoadScript> */}
-        </ModalProvider>
-      </UserProvider>
-    </AuthProvider>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <UserProvider>
+          <ModalProvider>
+            {/* <LoadScript googleMapsApiKey="YOUR_GOOGLE_MAPS_API_KEY"> */}
+            {/* <GoogleMapsLoader> */}
+            <Router>
+              <NavBar />
+              <div style={{ marginBottom: '20px' }} />
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/user/:userId" element={<AccountPage />} />
+                <Route path="/bill/:id" element={<BillDetailPage />} />
+              </Routes>
+            </Router>
+            {/* </GoogleMapsLoader> */}
+            {/* </LoadScript> */}
+          </ModalProvider>
+        </UserProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
