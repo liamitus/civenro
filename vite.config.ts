f2b38a5a2ef6b63f3ff58d90e5573a358e096eab
Liam Howell <liammm@gmail.com>
2025-01-04 10:25:06 -0500

    refactor: update Vite server configuration to conditionally apply proxy settings based on NODE_ENV

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,16 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://backend:5001',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  server:
+    process.env.NODE_ENV !== 'production'
+      ? {
+          proxy: {
+            '/api': {
+              target: 'http://backend:5001',
+              changeOrigin: true,
+              secure: false,
+            },
+          },
+        }
+      : {},
 });
